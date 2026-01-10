ee80e78d9cdc3e8b9cbe916e66accf227970da69
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2026-01-11 02:35:10 +0530

    Update Keycloak URL configuration to use environment variable for better flexibility

--- app/core/config.py
+++ app/core/config.py
@@ -23,8 +23,8 @@ class Settings(BaseSettings):
     MONGODB_DB_NAME: str = os.getenv("BLOG_MONGODB_DB_NAME", "")
 
     # Keycloak settings
-    KEYCLOAK_URL: str = "http://localhost:8080"
-    REALM: str = "master"
+    KEYCLOAK_URL: str = os.getenv("KEYCLOAK_URL", "https://aistudentchapter.lk/keycloak")
+    REALM: str = os.getenv("KEYCLOAK_REALM", "master")
     CLIENT_ID: str = "blogs-service"  # Custom client used for blog service (separation of concerns)
     CLIENT_SECRET: str = os.getenv("BLOG_CLIENT_SECRET", "")
 
